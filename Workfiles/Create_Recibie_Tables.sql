01555252eb07f9424d1ddb97faecabc2d816d832
DasMaloon <buhlmann.marlon@gmail.com>
2025-10-02 09:32:18 +0200

    idk

--- Workfiles/Create_Recibie_Tables.sql
+++ Workfiles/Create_Recibie_Tables.sql
@@ -10,3 +10,4 @@ CREATE TABLE RECIPE (
 /* Add dummy Zutat */
 
 INSERT INTO RECIPE (REZEPTID, REZEPTNAME, BESCHREIBUNG) VALUES (99999999,'TestRezept','Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
+INSERT INTO RECIPE (REZEPTID, REZEPTNAME, BESCHREIBUNG) VALUES (99999998,'TestRezept2','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vehicula, nunc ut facilisis fringilla, ligula enim efficitur ligula, nec tincidunt enim erat a nunc. Sed id dui ac nisi convallis facilisis. Donec euismod, nisi vel consectetur interdum, nisl nisi cursus nunc, eget tincidunt nunc nisl euismod nunc.');
